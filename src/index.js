9264427b505ff7c0f5793810b257cc0cde280293
Jonathan Chan <tmxistz@gmail.com>
2020-04-27 14:04:28 +0900

    refactored graphql

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 // index.js
 // This is the main entry point of our application
+const express = require("express")
+const { ApolloServer } = require("apollo-server-express")
+require("dotenv").config()
+
+const db = require("./db")
+const models = require("./models")
+const typeDefs = require("./schema")
+const resolvers = require("./resolvers")
+
+const port = process.env.PORT  || 4000
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+const app = express()
+db.connect(
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME
+)
+
+const server = new ApolloServer(
+  {
+    typeDefs, 
+    resolvers,
+    context: () => {
+      return { models }
+    }
+  }
+)
+server.applyMiddleware({ app, path: "/api"})
+
+app.listen(
+  {port}, 
+  () =>  console.log(`GraphQL server running at http://localhost:${port}${server.graphqlPath}`)
+)
---
+++ src/schema.js
@@ -0,0 +1,18 @@
+const {  gql } = require("apollo-server-express")
+
+module.exports = gql`
+  type Query {
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+`
